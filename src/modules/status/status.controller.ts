03fd252d2c99bb020fab36a921ab764bf05fc61f
4Ply <gamer.4ply@gmail.com>
2024-05-05 22:50:00 +0200

    chore(status): switch to modern formatting style
    https://docs.advntr.dev/minimessage/format.html

--- src/modules/status/status.controller.ts
+++ src/modules/status/status.controller.ts
@@ -28,28 +28,28 @@ export const getStatus = catchAsync(async (_req: Request, res: Response) => {
 
   const status = [
     {
-      header: `§3Instances (${instances.length} total)`,
+      header: `<dark_aqua>Instances (${instances.length} total)</dark_aqua>`,
       lines: [
         {
-          key: '§aAvailable',
+          key: '<green>Available</green>',
           value: instances.filter((instance) => {
             return instance.state === InstanceStates.AVAILABLE && !instance.requiresRebuild && instance.activePlayers === 0;
           }).length,
         },
         {
-          key: '§bIn use',
+          key: '<aqua>In use</aqua>',
           value: instances.filter((instance) => {
             return instance.state === InstanceStates.IN_USE && !instance.requiresRebuild;
           }).length,
         },
         {
-          key: '§6Awaiting player',
+          key: '<gold>Awaiting player</gold>',
           value: instances.filter((instance) => {
             return [InstanceStates.AWAITING_PLAYER, InstanceStates.RESERVED].includes(instance.state) && !instance.requiresRebuild;
           }).length,
         },
         {
-          key: '§cRebuilding',
+          key: '<red>Rebuilding</red>',
           value: instances.filter((instance) => {
             return [InstanceStates.BUILDING, InstanceStates.UNREACHABLE].includes(instance.state) || instance.requiresRebuild;
           }).length,
@@ -57,26 +57,26 @@ export const getStatus = catchAsync(async (_req: Request, res: Response) => {
       ],
     },
     {
-      header: `§3Players (${players.length + stalePlayers.length} stored in DB)`,
+      header: `<dark_aqua>Players (${players.length + stalePlayers.length} stored in DB</dark_aqua>)`,
       lines: [
         {
-          key: '§aIn game',
+          key: '<green>In game</green>',
           value: players.filter((player) => player.state === QueueStates.IN_DUNGEON).length,
         },
         {
-          key: '§6In queue',
+          key: '<gold>In queue</gold>',
           value: players.filter((player) => player.state === QueueStates.IN_QUEUE).length,
         },
         {
-          key: '§6In transit',
+          key: '<gold>In transit</gold>',
           value: players.filter((player) => player.state === QueueStates.IN_TRANSIT_TO_DUNGEON).length,
         },
         {
-          key: '§bIn lobby',
+          key: '<aqua>In lobby</aqua>',
           value: players.filter((player) => player.state === QueueStates.IN_LOBBY).length,
         },
         {
-          key: '§cOffline',
+          key: '<red>Offline</red>',
           value: stalePlayers.length,
         },
       ],
