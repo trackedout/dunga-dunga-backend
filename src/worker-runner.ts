87533e3a4bffc9ee5b5d454f9dca9211e676b21c
4Ply <gamer.4ply@gmail.com>
2024-12-22 21:24:56 +0200

    Run worker and web app as separate processes, and scale web app to 2 processes

--- src/worker-runner.ts
+++ src/worker-runner.ts
@@ -1,26 +1,36 @@
 import mongoose from 'mongoose';
-import app from './app';
+import worker from './worker';
 import config from './config/config';
 import logger from './modules/logger/logger';
 
-let server: any;
+function sleep(ms: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+function runWorker() {
+  worker
+    .run()
+    .then(async () => {
+      await sleep(5000);
+      runWorker();
+    })
+    .catch(async (err) => {
+      logger.error('Error running background worker:', err);
+      await sleep(5000);
+      runWorker();
+    });
+}
 
 mongoose.connect(config.mongoose.url).then(() => {
   logger.info('Connected to MongoDB');
-  server = app.listen(config.port, () => {
-    logger.info(`Listening to port ${config.port}`);
-  });
+
+  runWorker();
 });
 
 const exitHandler = () => {
-  if (server) {
-    server.close(() => {
-      logger.info('Server closed');
-      process.exit(1);
-    });
-  } else {
-    process.exit(1);
-  }
+  process.exit(1);
 };
 
 const unexpectedErrorHandler = (error: string) => {
@@ -33,7 +43,4 @@ process.on('unhandledRejection', unexpectedErrorHandler);
 
 process.on('SIGTERM', () => {
   logger.info('SIGTERM received');
-  if (server) {
-    server.close();
-  }
 });
